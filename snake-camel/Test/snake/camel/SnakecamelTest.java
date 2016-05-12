114d8352803c49d601956483b9d518c389120302
KasumiIwamoto <kpe.359@icloud.com>
2016-05-12 19:59:34 +0900

    second commit

--- snake-camel/Test/snake/camel/SnakecamelTest.java
+++ snake-camel/Test/snake/camel/SnakecamelTest.java
@@ -1,14 +1,26 @@
 package snake.camel;
 
 import static org.junit.Assert.*;
-
+import static org.hamcrest.CoreMatchers.is;
 import org.junit.Test;
+
+import snakecamel.SnakeCamelUtil;
 
 public class SnakecamelTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void スネークケースの文字列をキャメルケースの文字列に変換する() {
+		SnakeCamelUtil snake = new SnakeCamelUtil();
+		String expected = "AbcDef";
+		String actual = snake.snakeToCamelcase("abc_def");
+		assertThat(actual,is(expected));
+	}
+	@Test
+	public void キャメルケースの文字列をスネークケースの文字列に変換する() {
+		SnakeCamelUtil camel = new SnakeCamelUtil();
+		String expected = "abc_def";
+		String actual = camel.camelToSnakecase("AbcDef");
+		assertThat(actual,is(expected));
 	}
 
 }
